e71d22554a3dfbc8382b96507f2f16ea7fa641e5
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-07 11:32:42 +0100

    created index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const organisationRoutes = require('./routes/organisation');
+const userRoutes = require('./routes/user');
+
+// Initialize the Express app
+const app = express();
+
+// Set the port from environment variables or default to 3000
+const PORT = process.env.PORT || 3000;
+
+// Add support for Cross-Origin Resource Sharing (CORS)
+app.use(cors());
+app.options('*', cors());
+
+// Configure body parser middleware to handle JSON and URL encoded data
+app.use(bodyParser.json({ limit: '50mb' })); // support json encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Define routes
+app.use('/auth', authRoutes);
+app.use('/api/organisations', organisationRoutes);
+app.use('/api/users', userRoutes);
+
+// Start the server and listen on the defined port
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Export the server for testing or other purposes
+module.exports = server;
--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = server;
